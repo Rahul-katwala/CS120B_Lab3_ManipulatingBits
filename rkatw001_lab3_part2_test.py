600718b85d39ae315f83d22ec76e7f1f8db25eee
Rahul-katwala <67291221+Rahul-katwala@users.noreply.github.com>
2021-10-09 15:53:13 -0700

    Create rkatw001_lab3_part2_test.py
    Completed part 2

---
+++ rkatw001_lab3_part2_test.py
@@ -0,0 +1,31 @@
+tests = [ {'description': 'PINA:  0x03',
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 1 } ],
+    'expected': [('PORTC',0x70)],
+    },
+
+ {'description': 'PINA: 0X06',
+    'steps': [ {'inputs': [('PINA',0x06)], 'iterations': 1 } ],
+    'expected': [('PORTC',0x38)],
+    },
+
+ {'description': 'PINA = 0x0F',
+    'steps': [ {'inputs': [('PINA',0x0F)], 'iterations': 1 } ],
+    'expected': [('PORTC',0x3F)],
+    },
+
+ {'description': 'PINA = 0x0A',
+    'steps': [ {'inputs': [('PINA',0x0A)], 'iterations': 1 } ],
+    'expected': [('PORTC',0x3E)],
+    },
+
+
+
+#    {'description': 'This test will run second.',
+ #   'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
+  #      {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
+   #     {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
+    #    {'inputs': [('PIN',<val>)], 'time': 600}, ],
+   # 'expected': [('PORT',<val>)],
+  #  },
+    ]
+
